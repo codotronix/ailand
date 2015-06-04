7440f85a5788ff39c3607b14edb3a1707578f7b3
codotronix <codotronix@gmail.com>
2015-06-05 02:06:52 +0530

    login page created

--- js/AScript.js
+++ js/AScript.js
@@ -1,22 +1,34 @@
 /****** The Main App *************/
 var mainApp = angular.module('mainApp', ['ngRoute']);
 
-/****** The Main Controlelr *****/
+/*** The Shared Service ***/
+mainApp.service('sharedVars', function () {
+   var sharedVars = {};
+   sharedVars.currentPage = 'index';
+   return sharedVars;
+});
+
+/****** The Main Controller *****/
 mainApp.controller('mainCtrl', ['$scope', function ($scope) {
 	$scope.topMenu = ['Menu Item 1', 'Menu Item 2', 'Menu Item 3', 'Menu Item 4', 'Menu Item 5', 'Menu Item 6'];
-   $scope.leftMenu = ['Menu Item 1', 'Menu Item 2', 'Menu Item 3', 'Menu Item 4', 'Menu Item 5', 'Menu Item 6'];
+   $scope.leftMenu = ['Side Item 1', 'Side Item 2', 'Side Item 3', 'Side Item 4', 'Side Item 5', 'Side Item 6'];
+   
 }]);
 
 /****** lets do the routing ***********/
 mainApp.config(['$routeProvider',
          function($routeProvider) {
             $routeProvider.
-               when('/friends', {
+               when('/login', {
+                  templateUrl: 'template/login.html',
+                  controller: 'loginCtrl'
+               })
+               .when('/friends', {
                   templateUrl: 'template/people.html',
                   controller: 'peopleCtrl'
                })
                .when('/', {
-               		redirectTo: '/friends'
+               		redirectTo: '/login'
                })
                .otherwise({
                		redirectTo: '/'
@@ -25,5 +37,13 @@ mainApp.config(['$routeProvider',
 
 /***** Routing Controllers ***********/
 mainApp.controller('peopleCtrl', ['$scope', function ($scope) {
+   $('body').removeClass().addClass('generalPage');
 	$scope.items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8', 'Item 9', 'Item 10', 'Item 11', 'Item 12'];
+   
 }]);
+
+/***** Login Controller *************/
+mainApp.controller('loginCtrl', ['$scope', function ($scope) {
+   $('body').removeClass().addClass('loginPage');
+}]);
+
