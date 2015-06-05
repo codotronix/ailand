6fdb9d52a743c9a4a94e8747ce53357deed7c9ff
codotronix <codotronix@gmail.com>
2015-06-05 20:28:15 +0530

    deleted the ndoe files

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-	app = express();
-
-app.get('/', function(req, res){
-	res.send('Hello Node Express');
-});
-
-app.listen(9090);
-console.log('Node Express Server running at localhost:9090');
